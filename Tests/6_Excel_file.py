15ab7b35515639debfcc15fff6f87dcf0f0cb2cb
dongeorge77 <dongeorge95@gmail.com>
2019-03-10 10:11:08 +0530

    xlrd

--- Tests/6_Excel_file.py
+++ Tests/6_Excel_file.py
@@ -1,6 +1,6 @@
 import xlrd
 
-file_loc="F:/Selenium/WorkBook.xlsx"
+file_loc="C:/Users/ADMIN/PycharmProjects/13th_Class_9_3_2019/Files/WorkBook.xlsx"
 wb=xlrd.open_workbook(file_loc)
 ws=wb.sheet_by_index(0)
 cell_val=ws.cell_value(0,1)
